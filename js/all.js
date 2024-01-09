b6320d8e9f8ed064af7780ea5b5b438a20636dc9
a31361548 <a31361548@gmail.com>
2024-01-09 15:40:21 +0800

    renew

--- js/all.js
+++ js/all.js
@@ -9,3 +9,42 @@ $('#race a').on('click', function () {
   $('#race a').removeClass('active')
   $(this).addClass('active')
 })
+
+// swiper---------------------------------------------------------------------
+new Swiper('.swiper', {
+  direction: 'horizontal',
+  loop: true,
+  speed: 1000,
+  spaceBetween: 15,
+  centeredSlides: true,
+  autoplay: {
+    delay: 5000
+  },
+  slidesPerView: 1,
+  effect: 'coverflow',
+  coverflowEffect: {
+    rotate: 50,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows: false
+  },
+  breakpoints: {
+    576: {
+      slidesPerView: 2
+    },
+    768: {
+      slidesPerView: 3
+    },
+    920: {
+      slidesPerView: 3
+    },
+    1200: {
+      slidesPerView: 4
+    }
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev'
+  }
+})
